a410544a96c3ee97b265296b0daec702fd86031c
bote795 <bote795@neo.tamu.edu>
2015-05-23 19:04:45 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#Project Information
+![logo](http://www.foreap.com/wp-content/uploads/2014/12/whatsnew8.png)
+##Goal
+create a web app that will be used to create and share events.
+
+##Description
+This web app will allow people to be able to create and share events with people. This will allow for easier way of knowing
+what your friends are up to and joining in. While later being able to poll time for events.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetboard::Application.config.secret_token = 'd2c85e7094f4e09b8ce086abb4b6539eb38257f7b3a64fdc0f2d52622a2cd4ffa3b028d5fff9f4a3f44a4cc0f171f92de502bbe37a8ccbe13aa720fe26826d30'
